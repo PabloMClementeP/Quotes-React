857a7c1b9ec154987d807e62ee6830d706001a8b
Pablo Clemente <pclementep@gmail.com>
2021-09-27 09:34:44 -0300

    getting quotes from api, using the method fetch

--- src/components/Breaking.jsx
+++ src/components/Breaking.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Timer from './Timer'
 
 const Breaking = () => {
 
-    const getQuote = ()=>{
-        console.log("obteniendo la cita de Braking Bad")
+    const [quote, setQuote] = useState({
+        text:"",
+        auth:""
+    });
+
+    
+    async function getQuote(){
+        try {
+            let res = await fetch("https://www.breakingbadapi.com/api/quote/random"),
+                json = await res.json();
+            if(!res.ok) throw {
+                status: res.status,
+                statusText: res.statusText
+            }
+            
+            setQuote({
+                text: json[0].quote,
+                auth: json[0].author
+            });
+        } catch (err) {
+            let message = err.statusText || "Error getting the quote";
+        } finally{
+
+        }
     }
+
+
+    useEffect(() =>{
+        getQuote();
+    },[]);
 
     return (
         <div>
             <h3>Breaking Bad Quote</h3>
             <Timer getQuote={getQuote}/>
+            <h3>{quote.text}</h3>
+            <h2>{quote.auth}</h2>
         </div>
     )
 }
--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,18 +1,42 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Timer from './Timer'
 
 const Quotes = () => {
 
+    const [quote, setQuote] = useState({
+        text:"",
+        auth:""
+    });
+
     // const [getingQuote, setGetingQuote] = useState(false);
 
     const getQuote = ()=>{
-        console.log("obteniendo la cita de Quotes API")
+        fetch("https://api.quotable.io/random")
+        .then((res) =>{ return res.ok ? res.json() : Promise.reject(res); })
+        .then(json =>{
+            // let quote = json.content,
+            //     author = json.author;
+            setQuote({
+                text: json.content,
+                auth: json.author
+            })
+            // $tip.innerHTML = `<h3>${quote}  <span>${author}</span></h3>`
+        }).catch(err=>{
+            let message = err.statusText || "Error getting the quote";
+            // $tip.innerHTML = `<h3>${err.status}: ${message}</h3>`
+        }).finally();
     }
+
+    useEffect(() =>{
+        getQuote();
+    },[]);
 
     return (
         <div>
             <h3>Quotes API</h3>
             <Timer getQuote={getQuote}/>
+            <h4>{quote.text}</h4>
+            <p>{quote.auth}</p>
         </div>
     )
 }
